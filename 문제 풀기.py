c3211016440d42574765489ac4b4a79396e6c407
AnnaloveTojji <88645338+AnnaloveTojji@users.noreply.github.com>
2021-08-23 10:47:14 +0900

    update 22번

--- 문제 풀기.py
+++ 문제 풀기.py
@@ -36,45 +36,80 @@
 #14
 #8
 #15
+a=128
+print(type(a))
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#1
+#print("Hello World")
+#2
+#print("Mary's cosmetics")
+#3
+#print("신씨가 소리질렀다. \"도둑이야 !\"")
+#4
+#print("C:\Windows")
+#5
+#안녕하세요.
+#만나서        반갑습니다.
+#6
+#오늘은 일요일
+#7
+#print("naver;kakao;sk;samsung")
+#8
+#print("naver/kakao/sk/samsung")
+#9
+#print("first",end="")
+#print("second")
+#10
+#print(5/3)
+#11
+#삼성전자=50000*10
+#print(samsunggunga)
+#12
+#시가총액="298조"
+#현재가=50000
+#PER=15.79
+#13
+#s="hello"
+#t="python"
+#print(s,end="")
+#print("!",end="")
+#print(t)
+#14
+#8
+#15
+#str
+#16
+#num_str="720"
+#num_str=int(num_str)
+#print(num_str)
+#17
+#num=100
+#num=str(num)
+#print(num)
+#18
+#f=15.79
+#f=float(f)
+#print(type(f))
+#19
+#year="2020"
+#year=int(year)
+#print(year-2,year-1,year)
+#20
+#a=48584
+#b=36
+#x=a*b
+#print(x)
+#21
+#letters='python'
+#print(letters[0],letters[2])
+#22
+#license_plate="24가 2210"
+#license_plate=license_plate[4:]
+#print(license_plate)
 
 
 
