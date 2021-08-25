d5936f2fcd4627f26f6f606dc826a55b52cf0582
AnnaloveTojji <88645338+AnnaloveTojji@users.noreply.github.com>
2021-08-25 11:46:08 +0900

    label
    Label 배워보기

---
+++ label.py
@@ -0,0 +1,10 @@
+from tkinter import*
+#main widow
+root=Tk()
+root.title("토찌")
+#widget.. label
+tojji=Label(root, text="Tojji", bg='green')
+#packing
+tojji.grid(row=10,column=16)
+#repeat반복
+root.mainloop()
