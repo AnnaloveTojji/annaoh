e00bb923d352c5f70cd918c4e1583a006c9e49b1
AnnaloveTojji <88645338+AnnaloveTojji@users.noreply.github.com>
2021-09-07 21:47:11 +0900

    선물

---
+++ 선물.py
@@ -0,0 +1,24 @@
+# 선물
+print("선생님께 선물까진 아닐 수 있지만 저의 코딩 성장 과정을 담은 프로젝트니 잘 봐주세요.")
+print("짧고 간단하게 한문장 정도로 정리 해 봤어요.")
+print("그럼 시작해 볼까요?")
+list1="파이썬 초보 Hello Codes! 첫날 배운 print를 비롯한 여러가지 기능."
+list2="for 반복문과 if등등..."
+list3="여러가지 기능들을 사용해서 문제도 풀고..."
+list4="int와 input도 아네!!!"
+list5="우와! 이제 len도 할 수 있네?!!"
+list6="이제 문제도 잘 풀 수 있어!"
+list7="나만의 타자게임 만들기!"
+list8="chatbot도 만들었네!"
+list9="내가 만든 더하기 계산기!"
+list10="더하기 사이클 틀렸어도 좋아!"
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+print(list5)
+print(list6)
+print(list7)
+print(list8)
+print(list9)
+print(list10)
