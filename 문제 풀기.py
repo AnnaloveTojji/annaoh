3c48a49e0daf1a27111c778a31dc34e1aff4457e
AnnaloveTojji <88645338+AnnaloveTojji@users.noreply.github.com>
2021-10-01 14:59:49 +0900

    Update 문제 풀기.py

--- 문제 풀기.py
+++ 문제 풀기.py
@@ -77,6 +77,38 @@ phone_number="010-1111-2222"
 print(phone_number)
 phone_number=phone_number.replace("-","")
 print(phone_number)
+#51
+movie_rank=['닥터 스트레인지','스플릿','럭키']
+print(movie_rank)
+#52
+movie_rank=['닥터 스트레인지','스플릿','럭키']
+movie_rank.append("배트맨")
+#53
+movie_rank=['닥터 스트레인지','스플릿','럭키','배트맨']
+movie_rank.insert(1,"슈퍼맨")
+#54
+movie_rank=['닥터 스트레인지','슈퍼맨','럭키','배트맨']
+del movie_rank[3]
+#55
+movie_rank=['닥터 스트레인지','슈퍼맨','배트맨']
+del movie_rank[2]
+#56
+lang1=['C','C++','JAVA']
+lang2=['Python','Go','C#']
+langs=[]
+langs=lang1+lang2
+#57
+nums=[1,2,3,4,5,6,7]
+print("max",max(nums))
+print("min",min(nums))
+#58
+print(sum(nums))
+#59
+cook=['피자','김밥','만두','양념치킨','족발','피자','김치만두','쫄면','소세지','라면','팥빙수','김치전']
+print(len(cook))
+#60
+nums=[1,2,3,4,5]
+print(sum(nums)/len(nums))
 
 
 
