ba915677dd4cb714b4e6e2f2871295a3117fe5dc
AnnaloveTojji <88645338+AnnaloveTojji@users.noreply.github.com>
2021-10-08 15:59:08 +0900

    enroenro

---
+++ 다이얼.py
@@ -0,0 +1,10 @@
+two=["A","B","C"]
+three=["D","E","F"]
+four=["G","H","I"]
+five=["J","K","L"]
+six=["M","N","O"]
+seven=["P","Q","R"]
+eight=["S","T","U"]
+nine=["V","W","X"]
+alphago=input().split()
+ 
