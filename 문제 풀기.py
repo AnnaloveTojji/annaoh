2ee717218106e685e2c9c3ecb686be49d9d1b5d1
AnnaloveTojji <88645338+AnnaloveTojji@users.noreply.github.com>
2021-10-14 22:14:32 +0900

    Update 문제 풀기.py

--- 문제 풀기.py
+++ 문제 풀기.py
@@ -1,76 +1,76 @@
 #1
-#print("Hello World")
+print("Hello World")
 #2
-#print("Mary's cosmetics")
+print("Mary's cosmetics")
 #3
-#print("신씨가 소리질렀다. \"도둑이야 !\"")
+print("신씨가 소리질렀다. \"도둑이야 !\"")
 #4
-#print("C:\Windows")
+print("C:\Windows")
 #5
-#안녕하세요.
-#만나서        반갑습니다.
+안녕하세요.
+만나서        반갑습니다.
 #6
-#오늘은 일요일
+오늘은 일요일
 #7
-#print("naver;kakao;sk;samsung")
+print("naver;kakao;sk;samsung")
 #8
-#print("naver/kakao/sk/samsung")
+print("naver/kakao/sk/samsung")
 #9
-#print("first",end="")
-#print("second")
+print("first",end="")
+print("second")
 #10
-#print(5/3)
+print(5/3)
 #11
-#삼성전자=50000*10
-#print(samsunggunga)
+삼성전자=50000*10
+print(samsunggunga)
 #12
-#시가총액="298조"
-#현재가=50000
-#PER=15.79
+시가총액="298조"
+현재가=50000
+PER=15.79
 #13
-#s="hello"
-#t="python"
-#print(s,end="")
-#print("!",end="")
-#print(t)
+s="hello"
+t="python"
+print(s,end="")
+print("!",end="")
+print(t)
 #14
-#8
+8
 #15
-#str
+str
 #16
-#num_str="720"
-#num_str=int(num_str)
-#print(num_str)
+num_str="720"
+num_str=int(num_str)
+print(num_str)
 #17
-#num=100
-#num=str(num)
-#print(num)
+num=100
+num=str(num)
+print(num)
 #18
-#f=15.79
-#f=float(f)
-#print(type(f))
+f=15.79
+f=float(f)
+print(type(f))
 #19
-#year="2020"
-#year=int(year)
-#print(year-2,year-1,year)
+year="2020"
+year=int(year)
+print(year-2,year-1,year)
 #20
-#a=48584
-#b=36
-#x=a*b
-#print(x)
+a=48584
+b=36
+x=a*b
+print(x)
 #21
-#letters='python'
-#print(letters[0],letters[2])
+letters='python'
+print(letters[0],letters[2])
 #22
-#license_plate="24가 2210"
-#license_plate=license_plate[4:]
-#print(license_plate)
+license_plate="24가 2210"
+license_plate=license_plate[4:]
+print(license_plate)
 #23
-#string="홀짝홀짝홀짝"
-#print(string[::2])
+string="홀짝홀짝홀짝"
+print(string[::2])
 #24
-#string="PYTHON"
-#print(string[::-1])
+string="PYTHON"
+print(string[::-1])
 #25 (정답확인/replace/배우지 않음)
 phone_number="010-1111-2222"
 #phone_number=phone_number.replace("-"," ")
