5fa7e4c5f27a35c819bd27cb634ba9c547e87441
AnnaloveTojji <88645338+AnnaloveTojji@users.noreply.github.com>
2021-10-15 15:14:18 +0900

    lala

--- ejdcl.py
+++ ejdcl.py
@@ -1,12 +1,12 @@
 #덩치
-people=int(input("몇명의 키를 비교할건가요?"))
-whole=[]
-for i in range(people):
-    weight,height=map(int,input("몸무게와 키를 차례로 입력하세요.(띄어쓰기 잊지 말고요!)").split())
-    whole.append((weight,height))
-for a in whole:
-    rank=1
-    for b in whole:
-        if a[0]<b[0] and a[1]<b[1]:
-            rank=rank-1
-            
+a=int(input("몇명의 덩치를 비교할 건가요?"))
+b=[]
+for c in range(a):
+    d,e=map(int,input("몸무게와 키를 한명씩 차례로 입력해주세요.").split())
+    b.append((d,e))
+for f in b:
+    g=1
+    for h in b:
+        if f[0]<h[0] and f[1]<h[1]:
+            g=g+1
+    print(g,end='')
