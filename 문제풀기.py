1178bb8758098714363b75dd9102acf87067489a
AnnaloveTojji <88645338+AnnaloveTojji@users.noreply.github.com>
2021-10-22 15:56:15 +0900

    화이팅 메시지
    오늘 하루도 반짝이는 토찌는 하뚜하뚜해 한 하루 보내기! 약속!

---
+++ 문제풀기.py
@@ -0,0 +1,54 @@
+#116
+#time=input("현재시간:")
+#if time[-2:]=="00":
+#   print("정각입니다.")
+#else:
+#    print("정각이 아닙니다.")
+#117
+#fruit = ["사과", "포도", "홍시"]
+#user = input("좋아하는 과일은?")
+#if user in fruit:
+#    print("정답입니다.")
+#else:
+#    print("오답입니다")
+#118
+#fruit = {"봄" : "딸기", "여름" : "토마토", "가을" : "사과"}
+#user = input("제가좋아하는계절은: ")
+#if user in fruit:
+#    print("정답입니다.")
+#else:
+#    print("오답입니다.")
+#119
+#fruit = {"봄" : "딸기", "여름" : "토마토", "가을" : "사과"}
+#user = input("제가좋아하는계절은: ")
+#if user in fruit:
+#    print("정답입니다.")
+#else:
+#   print("오답입니다.")
+#120
+#fruit = {"봄" : "딸기", "여름" : "토마토", "가을" : "사과"}
+#user = input("좋아하는 과일은?")
+#if user in fruit.values():
+#    print("정답입니다.")
+#else:
+#    print("오답입니다.")
+#121
+#a=input("")
+#if a.islower():
+#    print(a.upper())
+#else:
+#    print(a.lower())
+#122
+#score = input("score: ")
+#score = int(score)
+#if 81 <= score <= 100:
+#    print("grade is A")
+#elif 61 <= score <= 80:
+#    print("grade is B")
+#elif 41 <= score <= 60:
+#    print("grade is C")
+#elif 21 <= score <= 40:
+#    print("grade is D")
+#else:
+#
+print("grade is E")
---
+++ 통계값.py
@@ -0,0 +1,6 @@
+a=[]
+b=int(input("숫자를 입력해줘"))
+a.append(b)
+c=sum(b)
+d=len(b)
+e=c/b
