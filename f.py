e08320085a71070d5aaa5f399a8b6898b89a7190
AnnaloveTojji <88645338+AnnaloveTojji@users.noreply.github.com>
2021-11-08 14:28:42 +0900

    좌정2

--- f.py
+++ f.py
@@ -1,25 +1,8 @@
-counter=0
-total=0
-while counter<101:
-      total=total+counter
-      counter=counter+1
-print(total)
-
-my_list=[10, 99, 98, 85, 45, 59, 65, 66, 76, 12, 35, 13, 100, 80, 95]
-counter=0
-while counter<len(my_list):
-      if my_list[counter]==100:
-            print("There is 100 at index",counter)
-      counter=counter+1
-
-my_list=["Joe", "Sarah", "Mike", "Jess", "", "Matt", "", "Greg"]
-def name_adder(list):
-      newlist=[]
-      counter=0
-      while counter<len(list):
-            if list[counter]!="":
-                  newlist.append(list[counter])
-            counter=counter+1
-      return newlist
-print(name_adder(my_list))
-      
+number1= int(input())
+my_list = []
+for _ in range(number1):
+    x, y = map(int, input().split())
+    my_list.append([x,y])
+my_list.sort()
+for x, y in my_list:
+    print(x, y)
