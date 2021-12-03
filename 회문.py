89d8b7a44769520f127f6c0416a9e29fe591197b
AnnaloveTojji <88645338+AnnaloveTojji@users.noreply.github.com>
2021-12-03 15:58:24 +0900

    Update 회문.py

--- 회문.py
+++ 회문.py
@@ -1,8 +1,24 @@
-number=int(input())
-my_list1=[]
-for i in range(number):
-    words=input()
-    my_list1.append(words)
-my_list2=[]
-my_list3=[]
-my_list4=[]
+def is_palindrome(s):
+    return s == s[ : :-1]
+def almost_palindrome(s):
+    for i in range(len(s)):
+        new_string = s[ :i]+s[i+1: ]
+        result = is_palindrome(new_string)
+        if result == True:
+            return result
+    return False
+t = int(input())
+lst=[]
+for _ in range(t):
+    s = input()
+    lst.append(s)
+for s in lst:
+    result = is_palindrome(s)
+    if result == True:
+        print(0)
+    else:
+        result = almost_palindrome(s)
+        if result == True:
+            print(1)
+        else:
+            print(2)
